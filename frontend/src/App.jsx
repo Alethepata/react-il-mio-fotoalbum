2e1a92bd5c74f14d59a03332126a725842e600d0
Alethepata <alessia.patane@gmail.com>
2024-06-27 16:56:49 +0200

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/DefaultLayout";
+import Home from "./pages/Home";
+
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Rotte Pubbliche */}
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+
+        {/* Rotte Private */}
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
