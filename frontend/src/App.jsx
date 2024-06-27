4898611a876d0c8d79f57d9efe9f4a9b80477b2a
Alethepata <alessia.patane@gmail.com>
2024-06-27 18:57:08 +0200

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,40 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./layouts/DefaultLayout";
+
+import PrivatePage from "./middlewares/PrivatePage";
+
+import DefaultLayout from "./layouts/DefaultLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
 
   return (
     <BrowserRouter>
+      <AuthProvider>
+        
       <Routes>
 
         {/* Rotte Pubbliche */}
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
+        <Route path="/" element={<DefaultLayout/>}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Route>
 
         {/* Rotte Private */}
+        <Route path="/" element={
+          <PrivatePage>
+            <DashboardLayout/>
+          </PrivatePage>
+        }>
+          <Route path="dashboard" element={<Dashboard/>} />
+        </Route>
 
       </Routes>
+
+      </AuthProvider>
     </BrowserRouter>
   )
 }
