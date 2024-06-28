4cdb82c373ad115392a2836297897b0b1099d523
Alethepata <alessia.patane@gmail.com>
2024-06-28 12:30:33 +0200

    add api photos

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "../utils/axiosClient";
+
+
 function Dashboard() {
+
+    const [photos, setPhotos] = useState([]);
+
+    const getApi = async () => {
+        const photosData = await axios.get('/photos');
+
+        setPhotos(photosData.data);
+
+    }
+
+    useEffect(() => {
+        getApi();
+    }, [])
+
     return (
         <div className="container">
             <h1>Dashboard</h1>
