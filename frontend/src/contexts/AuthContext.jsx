0d54cdc221abbf19f41b1bd31d66e35056bdc5e0
Alethepata <alessia.patane@gmail.com>
2024-06-28 11:48:59 +0200

    edit AuthProvider

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,13 +1,58 @@
 import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useStorage from "../hooks/useStorage";
+import axios from "../utils/axiosClient";
 
 const AuthContext = createContext()
 
+
 const AuthProvider = ({ children }) => {
 
-    const [isLoggedIn, setIsLoggedIn] = useState(true)
+    const [user, setUser] = useStorage(null, 'user');
+
+    const [isLoggedIn, setIsLoggedIn] = useStorage(false, 'logged');
+
+    
+    const navigate = useNavigate();
+
+    const login = async (payload) => {
+        try{
+            const data = await axios.post('/auth/login', payload);
+
+            setUser(data.data);
+
+            localStorage.setItem('accessToken', data.token);
+
+            setIsLoggedIn(true);
+
+            navigate('/dashboard');
+
+        } catch (err) {
+            const errors = err.response.data.errors;
+            
+            const error = new Error(errors ? '' : err.response.data);
+            
+            error.errors = errors;
+            
+            throw error;
+ 
+
+        }
+    }
+
+    const logout = () => { 
+        setIsLoggedIn(false);
+
+        localStorage.removeItem('accessToken');
+
+        navigate('/login')
+    }
+
 
     const value = {
-        isLoggedIn
+        isLoggedIn,
+        login,
+        logout
     }
 
     return (
