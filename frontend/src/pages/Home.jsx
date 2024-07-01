4bbeb4bcb94962b00a7179ac8c5e924238de2ea0
Alethepata <alessia.patane@gmail.com>
2024-07-01 12:22:27 +0200

    add card

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
+import Card from "../components/partials/Card";
 import Message from "../components/partials/Message";
+import { usePhotos } from "../contexts/PhotosContext";
+
 
 function Home() {
+    const { photosHome } = usePhotos();
+
     return (
         <div className="container">
             <h1>Home</h1>
+            <div className="container-card d-flex flex-wrap justify-content-evenly gap-4">
+
+                {
+                    photosHome.map(photo => <Card key={`card_photo_dashboard_${photo.id}`} photo={photo} />)
+                }
+            
+            </div>
             <Message/>
         </div>
     )
